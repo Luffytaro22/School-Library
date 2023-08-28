71e09cc236d560daa4b04271a4e14d4a0cc09368
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-28 15:40:31 -0500

    Added Person class

---
+++ person.rb
@@ -0,0 +1,47 @@
+class Person
+  def initialize(name = "Unknown", age, parent_permission = true)
+    @id = Random.rand(1..1000);
+    @name = name;
+    @age = age;
+    @parent_permission = parent_permission;
+  end
+
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+  def age 
+    @age
+  end
+
+  def name=(value)
+    @name = value;
+  end
+
+  def age=(value)
+    @age = value;
+  end
+
+  def can_use_services?
+    if of_age?() || @parent_permission
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def of_age?
+    if @age >= 18 
+      true
+    else
+      false
+    end
+  end
+
+end
