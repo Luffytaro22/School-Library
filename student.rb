148298cfd38170c4b2e50f75784d7289aeb5a2b5
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-28 15:43:56 -0500

    Create a student class

---
+++ student.rb
@@ -0,0 +1,12 @@
+require './person.rb';
+
+class Student < Person
+  def initialize(classroom)
+    super(name, age, parent_permission);
+    @classroom = classroom;
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
