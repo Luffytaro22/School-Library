7297b5f6641acd7f8c657f030f66dd412c9485f1
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-29 14:52:17 -0500

    Modify te require with require_relative and eliminate the outputs

--- student.rb
+++ student.rb
@@ -1,4 +1,4 @@
-require './person'
+require_relative 'person'
 
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
@@ -10,22 +10,3 @@ class Student < Person
     '¯(ツ)/¯'
   end
 end
-
-student1 = Student.new(17, '2-E', 'Kaori', parent_permission: false)
-student2 = Student.new(24, '1-A', 'Manuel', parent_permission: true)
-puts "Student 1 id: #{student1.id}"
-puts "Student 1 name: #{student1.name}"
-student1.name = 'Stephania'
-puts "Student 1 new name: #{student1.name}"
-puts "Student 1 age: #{student1.age}"
-puts "Student 1 can use services: #{student1.can_use_services?}"
-student1.age = 25
-puts "Student 1 new age: #{student1.age}"
-puts "Student 1 can use services: #{student1.can_use_services?}"
-puts student1.play_hooky
-puts "\n"
-puts "Student 2 id: #{student2.id}"
-puts "Student 2 name: #{student2.name}"
-puts "Student 2 age: #{student2.age}"
-puts "Student 2 can use services: #{student2.can_use_services?}"
-puts student2.play_hooky
--- teacher.rb
+++ teacher.rb
@@ -1,4 +1,4 @@
-require './person'
+require_relative 'person'
 
 class Teacher < Person
   def initialize(age, specialization, name = 'Unknown')
@@ -10,16 +10,3 @@ class Teacher < Person
     true
   end
 end
-
-teacher1 = Teacher.new(32, 'Physics')
-teacher2 = Teacher.new(50, 'Language', 'Mario')
-
-puts "Teacher 1 id: #{teacher1.id}"
-puts "Teacher 1 name: #{teacher1.name}"
-puts "Teacher 1 age: #{teacher1.age}"
-puts "Can use service: #{teacher1.can_use_services?}"
-puts "\n"
-puts "Teacher 2 id: #{teacher2.id}"
-puts "Teacher 2 name: #{teacher2.name}"
-puts "Teacher 2 age: #{teacher2.age}"
-puts "Can use service: #{teacher2.can_use_services?}"
