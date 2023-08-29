07967bcfc69e58310153fcb39e354f7faf102c6f
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-29 14:49:31 -0500

    Add a Trimmer Decorator

--- main.rb
+++ main.rb
@@ -17,3 +17,15 @@ class CapitalizeDecorator < Decorator
     @nameable.correct_name.capitalize
   end
 end
+
+# Trimmer Decorator
+class TrimmerDecorator < Decorator
+  def correct_name
+    original_name = @nameable.correct_name
+    if original_name.length > 10
+      original_name[0..9]  # Trim the name to maximum 10 characters
+    else
+      original_name
+    end
+  end
+end
