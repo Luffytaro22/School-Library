3fde558e3c6687955b415671375a89ad4f36d9e2
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-30 14:08:27 -0500

    Create a method to add students to the classroom

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,13 @@
 class Classroom
   def initialize(label)
     @label = label
+    @students = []
   end
 
-  attr_accessor :label
+  attr_accessor :label, :students
+
+  def add_student(student)
+    students << student
+    student.classroom = self
+  end
 end
