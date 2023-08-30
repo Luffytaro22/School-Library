37d4822fee277cd2abdd23d67e0bc407f4e16e98
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-30 14:23:18 -0500

    Add relations with person a book in Rental class

--- rental.rb
+++ rental.rb
@@ -1,7 +1,15 @@
 class Rental
-  def initialize(date)
+  @@all = []
+  def initialize(date, person, book)
     @date = date
+    @person = person
+    @book = book
+    @@all << self
   end
 
-  attr_accessor :date
+  attr_accessor :date, :person, :book
+
+  def self.all
+    @@all
+  end
 end
