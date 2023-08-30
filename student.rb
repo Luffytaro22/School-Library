408f941b5b9d29084b91b696791a6110d911d4cb
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-30 14:14:25 -0500

    Add the students to the classroom

--- student.rb
+++ student.rb
@@ -1,10 +1,14 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.add_student(self) if classroom
   end
+
+  attr_accessor :classroom
 
   def play_hooky
     '¯(ツ)/¯'
