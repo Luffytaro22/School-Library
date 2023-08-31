25352adf03abc00377f85ccf1543a472a6eeee47
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-31 09:34:31 -0500

    Modify the belongs-to relationship

--- student.rb
+++ student.rb
@@ -5,7 +5,7 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom&.add_student(self)
+    classroom.add_student(self) unless classroom.add_student.include?(self)
   end
 
   attr_accessor :classroom
