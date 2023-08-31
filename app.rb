28056ad7a95788ea350bd444870d3731b6a93fa6
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-31 10:13:08 -0500

    Create an App class

---
+++ app.rb
@@ -0,0 +1,26 @@
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'book'
+require_relative 'rental'
+
+class App
+  def list_books
+    @book
+  end
+
+  def create_student(age, classroom, name, parent_permission)
+    Student.new(age, classroom, name, parent_permission)
+  end
+
+  def create_teacher(age, specialization, name)
+    Teacher.new(age, specialization, name)
+  end
+
+  def create_book(title, author)
+    Book.new(title, author)
+  end
+
+  def create_rental(date, person, book)
+    Rental.new(date, person, book)
+  end
+end
