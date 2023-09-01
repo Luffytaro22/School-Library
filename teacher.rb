654581ed0b3a6b8c61423330fbfc3c183d1efb57
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-01 09:44:24 -0500

    Add a getter to the Teacher class

--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,10 @@ require_relative 'person'
 class Teacher < Person
   def initialize(age, specialization, name = 'Unknown')
     super(age, name)
-    @specializationi = specialization
+    @specialization = specialization
   end
+
+  attr_reader :age, :name, :specialization
 
   def can_use_services?
     true
