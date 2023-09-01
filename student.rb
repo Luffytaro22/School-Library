9f43a809c0ddcea0821a620fd687e6189e36ab5a
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-01 10:28:51 -0500

    Fixed a typo error and add the parent_permission to the getter

--- student.rb
+++ student.rb
@@ -5,10 +5,10 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom.add_student(self) unless classroom.add_student.include?(self)
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
-  attr_accessor :classroom
+  attr_accessor :classroom, :parent_permission
 
   def play_hooky
     '¯(ツ)/¯'
