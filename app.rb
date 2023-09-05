fbffb0e007be629d83b62a0ac0b47498bfca49e5
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-05 17:46:58 -0500

    Fixed linters

--- app.rb
+++ app.rb
@@ -8,26 +8,24 @@ require_relative 'classroom'
 class App
   def initialize
     create_data
-    create_file_if_not_exists("./data/people.json")
-    create_file_if_not_exists("./data/books.json")
-    create_file_if_not_exists("./data/rentals.json")
+    create_file_if_not_exists('./data/people.json')
+    create_file_if_not_exists('./data/books.json')
+    create_file_if_not_exists('./data/rentals.json')
     load_data_from_files
   end
 
   def create_data
-    if Dir.exist?("./data")
-      return
-    else
-      Dir.mkdir("./data")
-    end
+    return if Dir.exist?('./data')
+
+    Dir.mkdir('./data')
   end
 
   def load_data_from_files
-    @people = load_json_file("./data/people.json", [])
-    @books = load_json_file("./data/books.json", [])
-    @rentals = load_json_file("./data/rentals.json", [])
+    @people = load_json_file('./data/people.json', [])
+    @books = load_json_file('./data/books.json', [])
+    @rentals = load_json_file('./data/rentals.json', [])
   end
-  
+
   def load_json_file(file_path, default_value)
     file = File.open(file_path)
     file_data = file.read
@@ -39,35 +37,34 @@ class App
   end
 
   def create_file_if_not_exists(file_path)
-    unless File.exist?(file_path)
-      File.open(file_path, "w") do |file|
-      end
-    end
+    return if File.exist?(file_path)
+
+    File.open(file_path, 'w')
   end
 
   def list_books
     @books.each do |book|
-      puts "Title: '#{book["title"]}', Author: #{book["author"]}"
+      puts "Title: '#{book['title']}', Author: #{book['author']}"
     end
   end
 
   def list_people
     @people.each do |person|
-      if person["role"] == '[Teacher]'
-        puts "#{person["role"]} ID: #{person["id"]}, Name: #{person["name"]}, Age: #{person["age"]},
-        Specialization: #{person["specialization"]}"
+      if person['role'] == '[Teacher]'
+        puts "#{person['role']} ID: #{person['id']}, Name: #{person['name']}, Age: #{person['age']},
+        Specialization: #{person['specialization']}"
       else
-        puts "#{person["role"]} ID: #{person["id"]}, Name: #{person["name"]}, Age: #{person["age"]},
-        Classroom: #{person["classroom"]}, Parent Permission: #{person["permission"]}"
+        puts "#{person['role']} ID: #{person['id']}, Name: #{person['name']}, Age: #{person['age']},
+        Classroom: #{person['classroom']}, Parent Permission: #{person['permission']}"
       end
     end
   end
 
   def list_rentals_for_person(person_id)
-    filtered_rentals = @rentals.select { |obj| obj["id"] == person_id }
+    filtered_rentals = @rentals.select { |obj| obj['id'] == person_id }
     if filtered_rentals.length.positive?
       filtered_rentals.each do |rental|
-        puts "Date: #{rental["date"]}, Book: '#{rental["title"]}' by #{rental["author"]}"
+        puts "Date: #{rental['date']}, Book: '#{rental['title']}' by #{rental['author']}"
       end
     else
       puts 'No matching ID'
@@ -78,56 +75,56 @@ class App
     classroom_name = Classroom.new(classroom)
     student = Student.new(age, classroom_name, name, parent_permission: permission)
     student_input = {
-      "role" => '[Student]',
-      "id" => student.id,
-      "name" => student.name,
-      "age" => student.age,
-      "classroom" => classroom_name.label,
-      "permission" => student.parent_permission
+      'role' => '[Student]',
+      'id' => student.id,
+      'name' => student.name,
+      'age' => student.age,
+      'classroom' => classroom_name.label,
+      'permission' => student.parent_permission
     }
     @people << student_input
-    File.write("./data/people.json", JSON.pretty_generate(@people))
+    File.write('./data/people.json', JSON.pretty_generate(@people))
   end
 
   def create_teacher(age, specialization, name)
     teacher = Teacher.new(age, specialization, name)
     teacher_input = {
-      "role" => '[Teacher]',
-      "id" => teacher.id,
-      "name" => teacher.name,
-      "age" => teacher.age,
-      "specialization" => teacher.specialization
+      'role' => '[Teacher]',
+      'id' => teacher.id,
+      'name' => teacher.name,
+      'age' => teacher.age,
+      'specialization' => teacher.specialization
     }
     @people << teacher_input
-    File.write("./data/people.json", JSON.pretty_generate(@people))
+    File.write('./data/people.json', JSON.pretty_generate(@people))
   end
 
   def create_book(title, author)
     book = Book.new(title, author)
     book_input = {
-      "title" => book.title,
-      "author" => book.author
+      'title' => book.title,
+      'author' => book.author
     }
     @books << book_input
-    File.write("./data/books.json", JSON.pretty_generate(@books))
+    File.write('./data/books.json', JSON.pretty_generate(@books))
   end
 
   def list_books_with_index
     puts 'Select a book from the following list by number:'
     @books.each_with_index do |book, index|
-      puts "#{index}) Title: '#{book["title"]}', Author: #{book["author"]}"
+      puts "#{index}) Title: '#{book['title']}', Author: #{book['author']}"
     end
   end
 
   def list_people_with_index
     puts 'Select a person from the following list by number (not id):'
     @people.each_with_index do |person, index|
-      if person["role"] == '[Teacher]'
-        puts "#{index}) #{person["role"]} ID: #{person["id"]}, Name: #{person["name"]}, Age: #{person["age"]},
-        Specialization: #{person["specialization"]}"
+      if person['role'] == '[Teacher]'
+        puts "#{index}) #{person['role']} ID: #{person['id']}, Name: #{person['name']}, Age: #{person['age']},
+        Specialization: #{person['specialization']}"
       else
-        puts "#{index}) #{person["role"]} ID: #{person["id"]}, Name: #{person["name"]}, Age: #{person["age"]},
-        Classroom: #{person["classroom"]}, Parent Permission: #{person["permission"]}"
+        puts "#{index}) #{person['role']} ID: #{person['id']}, Name: #{person['name']}, Age: #{person['age']},
+        Classroom: #{person['classroom']}, Parent Permission: #{person['permission']}"
       end
     end
   end
@@ -137,12 +134,12 @@ class App
     book = @books[book_index]
     rental = Rental.new(date, person, book)
     rental_input = {
-      "id" => rental.person["id"],
-      "title" => rental.book["title"],
-      "author" => rental.book["author"],
-      "date" => rental.date
+      'id' => rental.person['id'],
+      'title' => rental.book['title'],
+      'author' => rental.book['author'],
+      'date' => rental.date
     }
     @rentals << rental_input
-    File.write("./data/rentals.json", JSON.pretty_generate(@rentals))
+    File.write('./data/rentals.json', JSON.pretty_generate(@rentals))
   end
 end
