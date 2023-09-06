5c5e471b08aa1e3e215b4785a585ce7115622b23
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-06 14:53:30 -0500

    Write unit tests for Rental class

---
+++ spec/rental_spec.rb
@@ -0,0 +1,18 @@
+require './rental.rb'
+require './person.rb'
+require './book.rb'
+
+describe Rental do
+  context "When testing Rental class" do
+    it 'Create a new rental with a new person and book' do
+      person1 = Person.new(45, 'Monica')
+      book1 = Book.new('Sunshine', 'Somebody')
+      rental1 = Rental.new('2023/11/25', person1, book1)
+
+      expect(rental1).to be_instance_of Rental
+      expect(rental1.date).to match '2023/11/25'
+      expect(rental1.person).to be_instance_of Person
+      expect(rental1.book).to be_instance_of Book
+    end
+  end
+end
